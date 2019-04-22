7ed458c9ff0d51f24b27253a5372e852b598073f
gab <gabdez98@gmail.com>
2019-04-22 19:30:45 +0200

    add all primary classes

---
+++ projet_POO/projet_POO/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projet_POO
+{
+    class Client
+    {
+        private int codeC;
+        private string nom;
+        private string prenom;
+    }
+}
---
+++ projet_POO/projet_POO/Vehicule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projet_POO
+{
+    abstract class Vehicule
+    {
+        private int codeV;
+        public string marque;
+        public int nbr_km;
+    }
+}
